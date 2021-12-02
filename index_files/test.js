6c858e82d8f42ce821aa0f9226206c4cbf366140
Alex E <teatime_dev@outlook.com>
2021-12-02 20:37:33 +1000

    obinugs obnugsuns

--- index_files/test.js
+++
@@ -1,4 +0,0 @@
-function dontclick() {
-    document.getElementById("dontclickelement").tagName = "h1";
-    document.getElementById("dontclickelement").innerHTML = "<iframe><body><h1>you just got iframe'd !!!!</h1></body></iframe>";
-}
